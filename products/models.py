b91d22cb0d84448a47333039e0e6b9bf30391598
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-04 16:34:01 -0700

    enable display of entity names in admin console
    entities were just displaying as their id before. this allows them to render by name.

--- products/models.py
+++ products/models.py
@@ -4,10 +4,18 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    
+    # Allows the admin page to display name vs just its id:
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+    
+    # Allows the admin page to display name vs just its id:
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -17,3 +25,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE) # cascade to avoid orphan
     # link many-to-many relationship with tags. one product has many tags, tags reused across products.
     tags = models.ManyToManyField(Tag)
+    
+    # Allows the admin page to display name vs just its id:
+    def __str__(self):
+        return self.name
