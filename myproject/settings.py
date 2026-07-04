9113bb85b1777c5f9af82b3fb302cd8ded649dd6
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-04 15:03:44 -0700

    Django create app boilerplate and register

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products', # register our created app in the project
 ]
 
 MIDDLEWARE = [
