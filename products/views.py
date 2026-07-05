d6a6863342e1d6ccf1c4a2a57de0b9a99796ee98
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-05 16:43:13 -0700

    added category and description search logic to view
    now the view searches both the product name and description for the search query item. Also added category filter logic

--- products/views.py
+++ products/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
+from django.db.models import Q
 
 # Create your views here.
 def product_list(request):
     # get all product objects using the Product model. 
     products = Product.objects.all().order_by('name')
+    categories = Category.objects.all()
     
     # get the search query:
     search_term = request.GET.get("search")
+    if search_term:
+        products = products.filter(Q(name__icontains=search_term) | Q(description__icontains=search_term))
+        
+    category_id = request.GET.get("category")
+    if category_id:
+        # chain across relationships, from product to category to its name. 
+        products = products.filter(category=category_id)
     
-    products = products.filter(name__icontains=search_term)
+    tags = request.GET.get("tags")
+    if tags:
+        pass
     # params are the request, the template file, plus the Product data as a python dict.
-    return render(request, 'products/product_list.html', {'products' : products})
+    return render(request, 'products/product_list.html', {'products' : products, 'categories' : categories})
