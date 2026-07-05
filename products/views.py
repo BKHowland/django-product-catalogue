183c4c3b011513d689d57a0995eab638376de5a3
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-05 15:20:10 -0700

    get products and pass to renderer
    we take the data from the model, which handles the DB calls, then we pass that to the template for rendering.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def product_list(request):
-    return render(request, 'products/product_list.html')
+    # get all product objects using the Product model. 
+    products = Product.objects.all().order_by('name')
+    # params are the request, the template file, plus the Product data as a python dict.
+    return render(request, 'products/product_list.html', {'products' : products})
