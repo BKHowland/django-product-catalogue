eef53170cc63169f3cf90dbf5f32a32a4e05d5d0
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-05 16:56:47 -0700

    Implemented tags filter in view
    and passed the list of possible tags to the template.

--- products/views.py
+++ products/views.py
@@ -1,25 +1,34 @@
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product, Category, Tag
 from django.db.models import Q
+
 
 # Create your views here.
 def product_list(request):
-    # get all product objects using the Product model. 
-    products = Product.objects.all().order_by('name')
+    # get all product objects using the Product model.
+    products = Product.objects.all().order_by("name")
     categories = Category.objects.all()
-    
+    tags = Tag.objects.all()
+
     # get the search query:
     search_term = request.GET.get("search")
     if search_term:
-        products = products.filter(Q(name__icontains=search_term) | Q(description__icontains=search_term))
-        
+        products = products.filter(
+            Q(name__icontains=search_term) | Q(description__icontains=search_term)
+        )
+
     category_id = request.GET.get("category")
     if category_id:
-        # chain across relationships, from product to category to its name. 
+        # chain across relationships, from product to category to its name.
         products = products.filter(category=category_id)
-    
-    tags = request.GET.get("tags")
-    if tags:
-        pass
+
+    tag_ids = request.GET.getlist("tags")
+    if tag_ids:
+        # we want to get all products where the tag id is in the tag ids requested.
+        products = products.filter(tags__id__in=tag_ids)
     # params are the request, the template file, plus the Product data as a python dict.
-    return render(request, 'products/product_list.html', {'products' : products, 'categories' : categories})
+    return render(
+        request,
+        "products/product_list.html",
+        {"products": products, "categories": categories, "tags": tags},
+    )
