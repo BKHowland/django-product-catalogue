1ace4bfcd5a09c1b9ba2166551b6fd821b3bac8d
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-05 15:59:47 -0700

    added search filter
    currently just searches for things contained in the product name.

--- products/views.py
+++ products/views.py
@@ -5,5 +5,10 @@ from .models import Product
 def product_list(request):
     # get all product objects using the Product model. 
     products = Product.objects.all().order_by('name')
+    
+    # get the search query:
+    search_term = request.GET.get("search")
+    
+    products = products.filter(name__icontains=search_term)
     # params are the request, the template file, plus the Product data as a python dict.
     return render(request, 'products/product_list.html', {'products' : products})
