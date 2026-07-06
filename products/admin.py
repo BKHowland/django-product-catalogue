198bc4659a60d2e1678a92066b82c5f46692f84f
Ben Howland <BenjaminKHowland@gmail.com>
2026-07-06 12:12:52 -0700

    updates products admin page
    admin page for products now matches the /products assignment page in functionality. Not required, but an easy nice to have.

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from . import models
 
+
+# since products have many relevant attributes, lets customize its view.
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Subclasses ModelAdmin to display product description and category,
+    show filters for category and tags, and display a search bar for name/description.
+    Improves usability and matches /products page functionality.
+    """
+
+    list_display = ("name", "description", "category")
+    list_filter = ("category", "tags")
+    search_fields = ("name", "description")
+
+
 # Register your models here.
 admin.site.register(models.Category)
 admin.site.register(models.Tag)
-admin.site.register(models.Product)
+admin.site.register(models.Product, ProductAdmin)
